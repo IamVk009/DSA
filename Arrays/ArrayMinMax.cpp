9a362ba4e85e3a3eddc4018e8d3a8a6129c62760
IamVk009 <imvk009@gmail.com>
2022-06-02 13:28:04 +0530

    Array Min Max

--- Arrays/ArrayMinMax.cpp
+++ Arrays/ArrayMinMax.cpp
@@ -2,23 +2,37 @@
 using namespace std;
 
 int getMin(int arr[], int size){
-    int min = INT_MAX;
+    // Way - 1
+    // int min = INT_MAX;
+    // for(int i = 0; i < size; i++){
+    //     if(arr[i] < min){
+    //         min = arr[i];
+    //     }
+    // }
+
+    // Way - 2
+    int minimum = INT_MAX;
     for(int i = 0; i < size; i++){
-        if(arr[i] < min){
-            min = arr[i];
-        }
+        minimum = min(minimum, arr[i]);
     }
-    return min;
+    return minimum;
 }
 
 int getMax(int arr[], int size){
-    int max = INT_MIN;
+    // Way - 1
+    // int max = INT_MIN;
+    // for(int i = 0; i < size; i++){
+    //     if(arr[i] > max){
+    //         max = arr[i];
+    //     }
+    // }
+
+    // Way - 2
+    int maximum = INT_MIN;
     for(int i = 0; i < size; i++){
-        if(arr[i] > max){
-            max = arr[i];
-        }
+        maximum = max(maximum, arr[i]);
     }
-    return max;
+    return maximum;
 }
 
 int main(){
