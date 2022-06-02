3fdeb0c987319c292ef5ac3b44cc107eae585c1a
IamVk009 <imvk009@gmail.com>
2022-06-02 12:16:58 +0530

    Array Initialisation

---
+++ Arrays/ArrayInit.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Declaration and Initialisation on same line.
+    int numbers[5] = {1,2,3,4,5};
+
+    // Initialise with Zero.
+    int zero[5] = {0};
+    for(int  i = 0; i < 5; i++){
+        cout << zero[i] << " ";
+    }
+    cout << endl;
+
+    // Initialise with any Value.
+    // https://stackoverflow.com/questions/1065774/initialization-of-all-elements-of-an-array-to-one-default-value-in-c
+    
+    int values[10];
+    fill_n(values, 10, -1);
+    for(int  i = 0; i < 10; i++){
+        cout << values[i] << " ";
+    }
+}
