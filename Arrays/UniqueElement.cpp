a0528975004e82accf3e9524d607cb292e980690
IamVk009 <imvk009@gmail.com>
2022-06-08 14:11:41 +0530

    New function added

--- Arrays/UniqueElement.cpp
+++ Arrays/UniqueElement.cpp
@@ -7,31 +7,32 @@ using namespace std;
     a ^ a = 0;
     0 ^ a = a;
 */
-// int getUnique(int arr[], int size){
-//     int unique = 0;
-//     for(int i = 0; i < size; i++){
-//         unique ^= arr[i];
-//     }
-//     return unique;
-// }
+
+int getUnique(int arr[], int size){
+    int unique = 0;
+    for(int i = 0; i < size; i++){
+        unique ^= arr[i];
+    }
+    return unique;
+}
 
 // Way - 2 (Without Using XOR)
 
-int getUnique2(int arr[], int size){
-    int count;
-    for(int i = 0; i < size; i++){
-        count = 0;
-        for(int j = 0; j < size; j++){
-            if(arr[i] == arr[j]){
-                count++;
-            }
-        }
-        if(count == 1){
-            return arr[i];
-        }
-    }
-    return 0;
-}
+// int getUnique2(int arr[], int size){
+//     int count;
+//     for(int i = 0; i < size; i++){
+//         count = 0;
+//         for(int j = 0; j < size; j++){
+//             if(arr[i] == arr[j]){
+//                 count++;
+//             }
+//         }
+//         if(count == 1){
+//             return arr[i];
+//         }
+//     }
+//     return 0;
+// }
 
 
 int main(){
@@ -44,5 +45,5 @@ int main(){
         cin >> numbers[i];
     }
 
-    cout << "Unique element in entered array = " << getUnique2(numbers, size);
+    cout << "Unique element in entered array = " << getUnique(numbers, size);
 }
