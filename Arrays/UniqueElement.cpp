416ab6d1abd5e5f1fb552d59cfd898f8d9be9420
IamVk009 <imvk009@gmail.com>
2022-06-08 14:09:41 +0530

    New function Added

--- Arrays/UniqueElement.cpp
+++ Arrays/UniqueElement.cpp
@@ -1,17 +1,38 @@
+// CodeStudio Question - 187
 #include<iostream>
 using namespace std;
 
+// Way - 1 (Using XOR)
 /*
     a ^ a = 0;
     0 ^ a = a;
 */
-int getUnique(int arr[], int size){
-    int unique = 0;
+// int getUnique(int arr[], int size){
+//     int unique = 0;
+//     for(int i = 0; i < size; i++){
+//         unique ^= arr[i];
+//     }
+//     return unique;
+// }
+
+// Way - 2 (Without Using XOR)
+
+int getUnique2(int arr[], int size){
+    int count;
     for(int i = 0; i < size; i++){
-        unique ^= arr[i];
+        count = 0;
+        for(int j = 0; j < size; j++){
+            if(arr[i] == arr[j]){
+                count++;
+            }
+        }
+        if(count == 1){
+            return arr[i];
+        }
     }
-    return unique;
+    return 0;
 }
+
 
 int main(){
     int numbers[100];
@@ -23,5 +44,5 @@ int main(){
         cin >> numbers[i];
     }
 
-    cout << "Unique element in entered array = " << getUnique(numbers, size);
+    cout << "Unique element in entered array = " << getUnique2(numbers, size);
 }
